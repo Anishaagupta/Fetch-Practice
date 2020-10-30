a87d30da964db5f7506795bcd878af16aad7edd4
Anishaagupta <anisha8057@gmail.com>
2020-10-30 19:24:04 +0530

    loading in functional components done

--- src/FunctionBased.js
+++ src/FunctionBased.js
@@ -1,21 +1,23 @@
 import React, { memo, useState, useEffect } from "react";
 import CountriesTable from "./CountriesTable";
+import Loader from './Loader';
 
 // import $ from "jquery";
 // import axios from "axios";
 
 let FunctionBasedCounter = () => {
-  const [countriesData, setCountriesData] = useState(null);
+  const [countriesData, setCountriesData] = useState({loading:false,data:[],error:''});
 
   //USe effect as componentDidMount
   useEffect(() => {
+    setCountriesData({loading:true,data:[],error:""});
     fetch("https://restcountries.eu/rest/v2/all")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setCountriesData(data);
-      })
-      .catch(console.error);
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setCountriesData({loading:false,data:data,error:""});
+    })
+    .catch(error=>setCountriesData({loading:false,data:[],error:error}));
 
     /* $.getJSON("https://restcountries.eu/rest/v2/all", (data) => {
       console.log(data);
@@ -31,7 +33,7 @@ let FunctionBasedCounter = () => {
       .catch(console.error); */
   }, []);
 
-  return countriesData ? <CountriesTable data={countriesData} /> : null;
+  return !countriesData.loading ? <CountriesTable data={countriesData.data} /> : <Loader/>;
 };
 
 FunctionBasedCounter = memo(FunctionBasedCounter);
